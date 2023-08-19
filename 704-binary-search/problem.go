7f0499c40f3010e9041f39f3a8723e597a1caf29
Andrey Nesterkin <nesterkin.am@gmail.com>
2023-08-19 15:21:04 +0300

    704. Binary Search [solved]

---
+++ 704-binary-search/problem.go
@@ -0,0 +1,21 @@
+package binarysearch
+
+func search(a []int, x int) int {
+	return reqursive_binary_search(a, 0, len(a) - 1, x)
+}
+
+func reqursive_binary_search(a []int, p, r, x int) int {
+	if (p > r){
+		return -1
+	}
+
+	q := (p + r) / 2
+	
+	if (a[q] == x){
+		return q
+	} else if (a[q] < x){
+		return reqursive_binary_search(a, q + 1, r, x)
+	} else {
+		return reqursive_binary_search(a, p, q - 1, x)
+	}
+}
---
+++ 704-binary-search/problem_test.go
@@ -0,0 +1,53 @@
+package binarysearch
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestBinarySearch(t *testing.T) {
+	testCases := []struct {
+		array []int
+		target int
+		want int
+		desc string
+	}{
+		{
+			array: []int{},
+			target: 1,
+			want: -1,
+			desc: "empty",
+		},
+		{
+			array: []int{ 1 },
+			target: 1,
+			want: 0,
+		},
+		{
+			array: []int{ 2 },
+			target: 1,
+			want: -1,
+			desc: "not found",
+		},
+		{
+			array: []int{ -1, 0, 1, 2 },
+			target: 0,
+			want: 1,
+		},
+		{
+			array: []int{ -1, 0, 1, 2 },
+			target: 2,
+			want: 3,
+		},
+	}
+
+	for i, tC := range testCases {
+		t.Run(fmt.Sprintf("%d. %v", i + 1, tC.desc), func(t *testing.T) {
+			got := search(tC.array, tC.target)
+
+			if got != tC.want {
+				t.Errorf("expected %v but %v", tC.want, got)
+			}
+		})
+	}
+}
