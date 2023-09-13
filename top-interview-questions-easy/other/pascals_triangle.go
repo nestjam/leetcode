4a488b5193fbe9cb372f04a0b7732d26d9dee615
Andrey Nesterkin <nesterkin.am@gmail.com>
2023-09-13 21:48:12 +0300

    other

---
+++ top-interview-questions-easy/other/pascals_triangle.go
@@ -0,0 +1,43 @@
+package other
+
+func generate(numRows int) [][]int {
+	if numRows == 1 {
+		return [][]int{{1}}
+	} 
+
+	t := make([][]int, numRows)
+	t[0] = []int{1}
+	t[1] = []int{1, 1}
+
+	for n := 2; n < numRows; n++ {
+		next := make([]int, n + 1)
+		next[0] = 1
+		next[len(next) - 1] = 1
+		for i := 1; i < len(next) - 1; i++ {
+			next[i] = t[n - 1][i] + t[n - 1][i - 1]
+		}
+		t[n] = next
+	}
+	return t
+}
+
+// with recursion
+/*
+func generate(numRows int) [][]int {
+    if numRows > 2 {
+		t := generate(numRows - 1)
+		prev := t[len(t) - 1]
+		next := make([]int, numRows)
+		next[0] = 1
+		next[len(next) - 1] = 1
+		for i := 1; i < numRows - 1; i++ {
+			next[i] = prev[i] + prev[i - 1]
+		}
+		t = append(t, next)
+		return t
+	} else if numRows == 2 {
+		return [][]int{{1}, {1, 1}}
+	}
+	return [][]int{{1}}
+}
+*/
---
+++ top-interview-questions-easy/other/pascals_triangle_test.go
@@ -0,0 +1,42 @@
+package other
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+func TestGenerate(t *testing.T) {
+	testCases := []struct {
+		numRows int
+		want  [][]int
+		desc  string
+	}{
+		{
+			numRows: 1,
+			want:  [][]int{{1}},
+		},
+		{
+			numRows: 2,
+			want:  [][]int{{1}, {1, 1}},
+		},
+		{
+			numRows: 3,
+			want:  [][]int{{1}, {1, 1}, {1, 2, 1}},
+		},
+		{
+			numRows: 5,
+			want:  [][]int{{1}, {1, 1}, {1, 2, 1}, {1, 3, 3, 1}, {1, 4, 6, 4, 1}},
+		},
+	}
+
+	for i, tC := range testCases {
+		t.Run(fmt.Sprintf("%d %v", i+1, tC.desc), func(t *testing.T) {
+			got := generate(tC.numRows)
+
+			if !reflect.DeepEqual(tC.want, got) {
+				t.Errorf("expected %v but %v", tC.want, got)
+			}
+		})
+	}
+}
