e271fb09d0d6065d41bd81f0fb0be67b98cdb32e
Andrey Nesterkin <nesterkin.am@gmail.com>
2023-09-23 19:56:46 +0300

    array

---
+++ top-interview-questions-easy/array/intersection_of_two_arrays_2.go
@@ -0,0 +1,35 @@
+package array
+
+func intersect(nums1 []int, nums2 []int) []int {
+    k := 0
+	for i := 0; i < len(nums1); i++ {
+		for j := k; j < len(nums2); j++ {
+			if nums1[i] == nums2[j] {
+				nums2[k], nums2[j] = nums2[j], nums2[k]			
+				k++
+				break
+			}
+		}	
+	}
+	return nums2[:k]
+}
+
+/*
+func intersect(nums1 []int, nums2 []int) []int {
+    if len(nums1) > len(nums2) {
+		nums1, nums2 = nums2, nums1
+	}
+	counters := make([]uint16, 1001)
+	for i := 0; i < len(nums2); i++ {
+		counters[nums2[i]]++
+	}
+	result := make([]int, 0, len(nums1))
+	for i := 0; i < len(nums1); i++ {
+		if counters[nums1[i]] > 0 {
+			result = append(result, nums1[i])
+			counters[nums1[i]]--
+		}
+	}
+	return result
+}
+*/
---
+++ top-interview-questions-easy/array/plus_one_test.go
@@ -0,0 +1,37 @@
+package array
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_plusOne(t *testing.T) {
+	type args struct {
+		digits []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{
+			args: args{ digits: []int{1}},
+			want: []int{2},
+		},
+		{
+			args: args{ digits: []int{}},
+			want: []int{1},
+		},
+		{
+			args: args{ digits: []int{9}},
+			want: []int{1, 0},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := plusOne(tt.args.digits); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("plusOne() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
