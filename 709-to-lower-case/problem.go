ea01ef75be3012adc1a9a58b19961804dc1f3ead
Andrey Nesterkin <nesterkin.am@gmail.com>
2024-05-19 16:55:04 +0300

    709. To Lower Case

---
+++ 709-to-lower-case/problem.go
@@ -0,0 +1,22 @@
+package tolowercase
+
+func toLowerCase(s string) string {
+    const (
+		d byte = 32
+		A byte = 65
+		Z byte = 90
+	)
+	r := make([]byte, len(s))
+
+	for i := 0; i < len(s); i++ {
+		r[i] = s[i]
+
+		if r[i] < A || r[i] > Z {
+			continue
+		}
+
+		r[i] += d
+	}
+
+	return string(r)
+}
