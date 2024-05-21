86e61ba68b3e703db3a1ffd6bd05bd7967e036bd
Andrey Nesterkin <nesterkin.am@gmail.com>
2024-05-21 09:50:45 +0300

    1431. Kids With the Greatest Number of Candies

---
+++ 1431-kids-with-the-greatest-number-of-candies/problem.go
@@ -0,0 +1,17 @@
+package kidswiththegreatestnumberofcandies
+
+func kidsWithCandies(candies []int, extraCandies int) []bool {
+    n := len(candies)
+	max := candies[0]
+	for i := 1; i < n; i++ {
+		if max < candies[i] {
+			max = candies[i]
+		} 
+	}
+
+	res := make([]bool, n)
+	for i := 0; i < n; i++ {
+		res[i] = (candies[i] + extraCandies) >= max
+	}
+	return res
+}
