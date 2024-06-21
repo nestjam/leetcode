9c40312711c37456538a90ab9a9e030b7256e476
Andrey Nesterkin <nesterkin.am@gmail.com>
2024-06-21 22:18:45 +0300

    perf: 509. Fibonacci Number

--- 509-fibonacci-number/problem.go
+++ 509-fibonacci-number/problem.go
@@ -1,12 +1,17 @@
 package fibonaccinumber
 
 func fib(n int) int {
-	switch n {
-	case 1:
-		return 1
-	case 0:
-		return 0
-	default:
-		return fib(n-1) + fib(n-2)
+	m := make(map[int]int, n)
+	m[0] = 0
+	m[1] = 1
+	calc(n, m)
+	return m[n]
+}
+
+func calc(n int, m map[int]int) {
+	if _, ok := m[n]; !ok {
+		calc(n-1, m)
+		calc(n-2, m)
+		m[n] = m[n-1] + m[n-2]
 	}
 }
