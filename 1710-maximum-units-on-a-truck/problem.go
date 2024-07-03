76cc392e758266562d3a2c73d0e2052a86a0fcfd
Andrey Nesterkin <nesterkin.am@gmail.com>
2024-07-03 09:53:15 +0300

    perf: 1710. Maximum Units on a Truck

--- 1710-maximum-units-on-a-truck/problem.go
+++ 1710-maximum-units-on-a-truck/problem.go
@@ -7,17 +7,18 @@ func maximumUnits(boxTypes [][]int, truckSize int) int {
 		return b[1] - a[1]
 	})
 
+	i := 0
 	size := 0
 	units := 0
 
-	for size < truckSize && len(boxTypes) > 0 {
-		n := min(truckSize-size, boxTypes[0][0])
-		units += n * boxTypes[0][1]
+	for size < truckSize && len(boxTypes) > i {
+		n := min(truckSize-size, boxTypes[i][0])
+		units += n * boxTypes[i][1]
 		size += n
-		boxTypes[0][0] -= n
+		boxTypes[i][0] -= n
 
-		if boxTypes[0][0] == 0 {
-			boxTypes = boxTypes[1:]
+		if boxTypes[i][0] == 0 {
+			i++
 		}
 	}
 
