2fcda6a7f55e5d0ba389b34896ec08dadfc91743
Andrey Nesterkin <nesterkin.am@gmail.com>
2024-11-22 01:05:19 +0300

    perf: 1514 use priority queue

--- 1514-path-with-maximum-probability/problem.go
+++ 1514-path-with-maximum-probability/problem.go
@@ -1,36 +1,37 @@
 package pathwithmaximumprobability
 
-import "math"
+import (
+	"container/heap"
+	"math"
+)
 
 func maxProbability(n int, edges [][]int, succProb []float64, start_node int, end_node int) float64 {
 	dist := make([]float64, n)
-	visited := make([]bool, n)
 
+	q := newDistHeap(n)
 	graph := initGraph(n, edges)
 	for i := 0; i < n; i++ {
 		dist[i] = -1
+		q.add(i, dist[i])
 	}
 	dist[start_node] = 1
+	q.set(start_node, dist[start_node])
 
-	for n > 0 {
-		u := getNext(dist, visited)
+	for len(q) > 0 {
+		d := q.get()
+		u := d.u
 
 		if u == -1 || u == end_node {
 			break
 		}
 
-		visited[u] = true
-		n--
-
 		neighbors := graph[u]
 		for i := 0; i < len(neighbors); i++ {
 			v := neighbors[i]
-			if visited[v.node] {
-				continue
-			}
 			alt := dist[u] * succProb[v.edge]
 			if alt > dist[v.node] {
 				dist[v.node] = alt
+				q.set(int(v.node), alt)
 			}
 		}
 	}
@@ -59,24 +60,6 @@ func initGraph(n int, edges [][]int) map[int]neighbors {
 	return g
 }
 
-func getNext(dist []float64, visited []bool) int {
-	max := float64(0)
-	n := -1
-
-	for i := 0; i < len(visited); i++ {
-		if visited[i] {
-			continue
-		}
-
-		if dist[i] >= max {
-			max = dist[i]
-			n = i
-		}
-	}
-
-	return n
-}
-
 func round(v float64) float64 {
 	d := float64(100_000)
 	return math.Round(v*d) / d
@@ -88,3 +71,54 @@ type neighbor struct {
 }
 
 type neighbors []neighbor
+
+type dist struct {
+	u int
+	p float64
+}
+
+type distHeap []dist
+
+func newDistHeap(capacity int) distHeap {
+	h := make(distHeap, 0, capacity)
+	heap.Init(&h)
+	return h
+}
+
+func (h distHeap) Len() int { return len(h) }
+
+func (h distHeap) Less(i, j int) bool {
+	return h[i].p > h[j].p
+}
+
+func (h distHeap) Swap(i, j int) { h[i], h[j] = h[j], h[i] }
+
+func (h *distHeap) Push(x any) {
+	*h = append(*h, x.(dist))
+}
+
+func (h *distHeap) Pop() any {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
+
+func (h *distHeap) add(u int, p float64) {
+	heap.Push(h, dist{u, p})
+}
+
+func (h *distHeap) get() dist {
+	return heap.Pop(h).(dist)
+}
+
+func (h distHeap) set(u int, p float64) {
+	for i := 0; i < len(h); i++ {
+		if h[i].u == u {
+			h[i].p = p
+			heap.Fix(&h, i)
+			return
+		}
+	}
+}
