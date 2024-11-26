86350bfbd34bfc5f761e2d0cec4da2b5cdee485e
Andrey Nesterkin <nesterkin.am@gmail.com>
2024-11-26 10:02:50 +0300

    perf: 1072. Flip Columns For Maximum Number of Equal Rows

--- 1072-flip-columns-for-maximum-number-of-equal-rows/problem.go
+++ 1072-flip-columns-for-maximum-number-of-equal-rows/problem.go
@@ -2,39 +2,25 @@ package flipcolumnsformaximumnumberofequalrows
 
 func maxEqualRowsAfterFlips(matrix [][]int) int {
 	maxCount := 0
-	flipColumns := make([]bool, len(matrix[0]))
-	flippedColumns := make(map[string]struct{}, len(matrix[0]))
+	flips := make(map[string]int, len(matrix[0]))
 
 	for i := 0; i < len(matrix); i++ {
-		r := matrix[i]
+		k := getKey(matrix[i])
+		flips[k]++
 
-		flipColumns[0] = true
-		for j := 1; j < len(r); j++ {
-			flipColumns[j] = r[0] == r[j]
-		}
-
-		key := getKey(flipColumns)
-
-		if _, ok := flippedColumns[key]; ok {
-			continue
-		}
-
-		flippedColumns[key] = struct{}{}
-
-		count := countEqualRows(matrix, flipColumns)
-		if count > maxCount {
-			maxCount = count
+		if flips[k] > maxCount {
+			maxCount = flips[k]
 		}
 	}
 
 	return maxCount
 }
 
-func getKey(columns []bool) string {
-	b := make([]byte, len(columns))
+func getKey(r []int) string {
+	b := make([]byte, len(r))
 
-	for i := 0; i < len(columns); i++ {
-		if columns[i] {
+	for i := 0; i < len(r); i++ {
+		if r[i] == r[0] {
 			b[i] = 1
 		} else {
 			b[i] = 0
@@ -42,40 +28,4 @@ func getKey(columns []bool) string {
 	}
 
 	return string(b)
-}
-
-func countEqualRows(matrix [][]int, flippedColumns []bool) int {
-	var count = 0
-
-	for i := 0; i < len(matrix); i++ {
-		if equalValues(matrix[i], flippedColumns) {
-			count++
-		}
-	}
-
-	return count
-}
-
-func equalValues(row []int, flippedColumns []bool) bool {
-	value := getValue(row[0], flippedColumns[0])
-
-	for i := 1; i < len(row); i++ {
-		if value != getValue(row[i], flippedColumns[i]) {
-			return false
-		}
-	}
-
-	return true
-}
-
-func getValue(value int, isFlipped bool) int {
-	if isFlipped {
-		if value == 0 {
-			return 1
-		} else {
-			return 0
-		}
-	}
-
-	return value
 }
