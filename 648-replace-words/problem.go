aa03d1762a74dc538b091cc3708a48fbae730905
Andrey Nesterkin <nesterkin.am@gmail.com>
2025-01-16 09:13:00 +0300

    refactor: 648. Replace Words

--- 648-replace-words/problem.go
+++ 648-replace-words/problem.go
@@ -4,66 +4,42 @@ import "strings"
 
 func replaceWords(dictionary []string, sentence string) string {
 	const space = ' '
-	root := buildTrie(dictionary)
+	trie := buildTrie(dictionary)
 
 	j := 0
 	newSentence := strings.Builder{}
 
 	for i := 0; i < len(sentence); i++ {
 		if sentence[i] == space {
-			word := replaceWord(sentence[j:i], root)
+			word := replaceWord(sentence[j:i], trie)
 			newSentence.WriteString(word)
 			newSentence.WriteByte(space)
 			j = i + 1
 		}
 	}
 
-	word := replaceWord(sentence[j:], root)
+	word := replaceWord(sentence[j:], trie)
 	newSentence.WriteString(word)
 
 	return newSentence.String()
 }
 
-func replaceWord(word string, node *trieNode) string {
-	i := 0
-	for ; i < len(word); i++ {
-		if node == nil {
-			continue
-		}
-
-		if child, ok := node.children[word[i]]; ok {
-			node = child
-			if node.isRoot {
-				i++
-				break
-			}
-		} else {
-			node = nil
-		}
+func replaceWord(word string, trie *trie) string {
+	if prefix, ok := trie.findShortestPrefix(word); ok {
+		return prefix
 	}
 
-	return word[:i]
+	return word
 }
 
-type trieNode struct {
-	isRoot   bool
-	children map[byte]*trieNode
-}
-
-func newTrieNode() *trieNode {
-	return &trieNode{
-		children: make(map[byte]*trieNode),
-	}
-}
-
-func buildTrie(dictionary []string) *trieNode {
-	root := newTrieNode()
+func buildTrie(dictionary []string) *trie {
+	trie := newTrie()
 
 	for i := 0; i < len(dictionary); i++ {
-		addWord(dictionary[i], root)
+		trie.add(dictionary[i])
 	}
 
-	return root
+	return trie
 }
 
 func addWord(word string, node *trieNode) {
@@ -75,5 +51,59 @@ func addWord(word string, node *trieNode) {
 		node = node.children[word[i]]
 	}
 
-	node.isRoot = true
+	node.isEnd = true
+}
+
+type trie struct {
+	root *trieNode
+}
+
+func newTrie() *trie {
+	return &trie{
+		root: newTrieNode(),
+	}
+}
+
+func (t *trie) add(s string) {
+	node := t.root
+
+	for i := 0; i < len(s); i++ {
+		if _, ok := node.children[s[i]]; !ok {
+			node.children[s[i]] = newTrieNode()
+		}
+
+		node = node.children[s[i]]
+	}
+
+	node.isEnd = true
+}
+
+func (t *trie) findShortestPrefix(s string) (string, bool) {
+	i := 0
+	node := t.root
+
+	for ; i < len(s); i++ {
+		if child, ok := node.children[s[i]]; ok {
+			node = child
+			if node.isEnd {
+				i++
+				return s[:i], true
+			}
+		} else {
+			break
+		}
+	}
+
+	return "", false
+}
+
+type trieNode struct {
+	isEnd    bool
+	children map[byte]*trieNode
+}
+
+func newTrieNode() *trieNode {
+	return &trieNode{
+		children: make(map[byte]*trieNode),
+	}
 }
