bd78091137de641d7c8b5907d3d264645d53e146
Andrey Nesterkin <nesterkin.am@gmail.com>
2025-01-16 09:22:43 +0300

    refactor: 648. Replace Words

--- 648-replace-words/problem.go
+++ 648-replace-words/problem.go
@@ -42,18 +42,6 @@ func buildTrie(dictionary []string) *trie {
 	return trie
 }
 
-func addWord(word string, node *trieNode) {
-	for i := 0; i < len(word); i++ {
-		if _, ok := node.children[word[i]]; !ok {
-			node.children[word[i]] = newTrieNode()
-		}
-
-		node = node.children[word[i]]
-	}
-
-	node.isEnd = true
-}
-
 type trie struct {
 	root *trieNode
 }
@@ -68,11 +56,14 @@ func (t *trie) add(s string) {
 	node := t.root
 
 	for i := 0; i < len(s); i++ {
-		if _, ok := node.children[s[i]]; !ok {
-			node.children[s[i]] = newTrieNode()
+		child, ok := node.findChild(s[i])
+
+		if !ok {
+			child = newTrieNode()
+			node.addChild(s[i], child)
 		}
 
-		node = node.children[s[i]]
+		node = child
 	}
 
 	node.isEnd = true
@@ -83,7 +74,7 @@ func (t *trie) findShortestPrefix(s string) (string, bool) {
 	node := t.root
 
 	for ; i < len(s); i++ {
-		if child, ok := node.children[s[i]]; ok {
+		if child, ok := node.findChild(s[i]); ok {
 			node = child
 			if node.isEnd {
 				i++
@@ -106,4 +97,13 @@ func newTrieNode() *trieNode {
 	return &trieNode{
 		children: make(map[byte]*trieNode),
 	}
+}
+
+func (t *trieNode) findChild(c byte) (*trieNode, bool) {
+	child, ok := t.children[c]
+	return child, ok
+}
+
+func (t *trieNode) addChild(c byte, child *trieNode) {
+	t.children[c] = child
 }
