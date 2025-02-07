6c81968613a1c201f6502ba60befac54fe15670f
Andrey Nesterkin <nesterkin.am@gmail.com>
2025-02-07 16:07:12 +0300

    refactor: 54. Spiral Matrix

--- 54-spiral-matrix/problem.go
+++ 54-spiral-matrix/problem.go
@@ -1,63 +1,60 @@
 package spiralmatrix
 
 func spiralOrder(matrix [][]int) []int {
+	const (
+		leftRight = 0
+		topDown   = 1
+		rightLeft = 2
+		downTop   = 3
+	)
+
 	n := len(matrix)
 	m := len(matrix[0])
 	line := make([]int, n*m)
 
-	readLeftRight(matrix, 0, 0, n, m, line, 0)
+	i, j, k := 0, 0, 0
+	direction := leftRight
+
+	for n > 0 && m > 0 {
+		switch direction {
+		case leftRight:
+			for l := 0; l < m; l++ {
+				line[k] = matrix[i][l+j]
+				k++
+			}
+			i++
+			j = j + m - 1
+			n--
+			direction = topDown
+		case topDown:
+			for l := 0; l < n; l++ {
+				line[k] = matrix[l+i][j]
+				k++
+			}
+			i = i + n - 1
+			j--
+			m--
+			direction = rightLeft
+		case rightLeft:
+			for l := 0; l < m; l++ {
+				line[k] = matrix[i][j-l]
+				k++
+			}
+			i--
+			j = j - m + 1
+			n--
+			direction = downTop
+		case downTop:
+			for l := 0; l < n; l++ {
+				line[k] = matrix[i-l][j]
+				k++
+			}
+			i = i - n + 1
+			j++
+			m--
+			direction = leftRight
+		}
+	}
 
 	return line
-}
-
-func readLeftRight(matrix [][]int, i, j, n, m int, line []int, k int) {
-	if n == 0 || m == 0 {
-		return
-	}
-
-	for l := 0; l < m; l++ {
-		line[k] = matrix[i][l+j]
-		k++
-	}
-
-	readTopDown(matrix, i+1, j+m-1, n-1, m, line, k)
-}
-
-func readTopDown(matrix [][]int, i, j, n, m int, line []int, k int) {
-	if n == 0 || m == 0 {
-		return
-	}
-	
-	for l := 0; l < n; l++ {
-		line[k] = matrix[l+i][j]
-		k++
-	}
-
-	readRightLeft(matrix, i+n-1, j-1, n, m-1, line, k)
-}
-
-func readRightLeft(matrix [][]int, i, j, n, m int, line []int, k int) {
-	if n == 0 || m == 0 {
-		return
-	}
-	
-	for l := 0; l < m; l++ {
-		line[k] = matrix[i][j-l]
-		k++
-	}
-
-	readDownTop(matrix, i-1, j-m+1, n-1, m, line, k)
-}
-
-func readDownTop(matrix [][]int, i, j, n, m int, line []int, k int) {
-	if n == 0 || m == 0 {
-		return
-	}
-	
-	for l := 0; l < n; l++ {
-		line[k] = matrix[i-l][j]
-		k++
-	}
-
-	readLeftRight(matrix, i-n+1, j+1, n, m-1, line, k)
 }
--- 54-spiral-matrix/problem_test.go
+++ 54-spiral-matrix/problem_test.go
@@ -42,7 +42,7 @@ func Test_spiralOrder(t *testing.T) {
 			args: args{
 				matrix: [][]int{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}},
 			},
-			want: []int{1,2,3,6,9,8,7,4,5},
+			want: []int{1, 2, 3, 6, 9, 8, 7, 4, 5},
 		},
 	}
 	for _, tt := range tests {
