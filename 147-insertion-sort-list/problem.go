d067a217ebca85081d19888e21dcb842bfe6362f
Andrey Nesterkin <nesterkin.am@gmail.com>
2025-02-20 10:16:32 +0300

    perf: 147. Insertion Sort List

--- 147-insertion-sort-list/problem.go
+++ 147-insertion-sort-list/problem.go
@@ -13,22 +13,19 @@ func insertionSortList(unsorted *ListNode) *ListNode {
 		node := unsorted
 		unsorted = unsorted.Next
 
-		node.Next, sorted = sorted, node
-
-		if sorted.Val <= sorted.Next.Val {
-			continue
+		cur := sorted
+		var prev *ListNode
+		for cur != nil && node.Val > cur.Val {
+			prev = cur
+			cur = cur.Next
 		}
 
-		cur := sorted
-		sorted = sorted.Next
-		var prev *ListNode
-		for cur.Next != nil && cur.Val > cur.Next.Val {
-			next := cur.Next
-			if prev != nil {
-				prev.Next = next
-			}
-			cur.Next, next.Next = next.Next, cur
-			prev = next
+		if prev == nil {
+			node.Next = cur
+			sorted = node
+		} else {
+			prev.Next = node
+			node.Next = cur
 		}
 	}
 
