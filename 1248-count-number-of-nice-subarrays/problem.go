9e7a4164f766099ba270e1eba904e80e0c17522b
Andrey Nesterkin <nesterkin.am@gmail.com>
2025-04-18 10:23:12 +0300

    1248. Count Number of Nice Subarrays

--- 1248-count-number-of-nice-subarrays/problem.go
+++ 1248-count-number-of-nice-subarrays/problem.go
@@ -1,25 +1,53 @@
 package countnumberofnicesubarrays
 
 func numberOfSubarrays(nums []int, k int) int {
-	subarraysCount := 0
+	count, n := 0, 0
+	odds := make([]int, 0)
+	j := 0
 
 	for i := 0; i < len(nums); i++ {
-		count := 0
+		if nums[i]%2 == 1 {
+			odds = append(odds, i)
+		}
 
-		for j := i; j < len(nums); j++ {
-			if nums[j]%2 == 1 {
-				count++
-			}
-
-			if count == k {
-				subarraysCount++
-			}
-
-			if count > k {
-				break
-			}
+		if len(odds) > k {
+			odds, j, n = countSubarrays(odds, i, j, k)
+			count += n
 		}
 	}
 
-	return subarraysCount
+	if len(odds) == k {
+		_, _, n = countSubarrays(odds, len(nums)-1, j, k)
+		count += n
+	}
+
+	return count
+}
+
+func countSubarrays(odds []int, i, j, k int) ([]int, int, int) {
+	l := odds[0] - j
+	r := 0
+	n := 0
+	if len(odds) > k {
+		r = odds[len(odds)-1] - odds[len(odds)-2] - 1
+	} else {
+		r = i - odds[len(odds)-1]
+	}
+
+	if l > 0 {
+		l++
+	}
+	if r > 0 {
+		r++
+	}
+
+	if l > 0 && r > 0 {
+		n = l * r
+	} else {
+		n = max(l, r, 1)
+	}
+
+	j = odds[0] + 1
+	odds = odds[1:]
+	return odds, j, n
 }
